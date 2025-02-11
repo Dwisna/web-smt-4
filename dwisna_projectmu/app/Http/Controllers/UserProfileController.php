04f063a06c827b460269d87106b38d24c7d64f4e
Dwisna <158534538+Dwisna@users.noreply.github.com>
2025-02-11 14:41:07 +0700

    Create UserProfileController.php

---
+++ dwisna_projectmu/app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserProfileController extends Controller
+{
+ public function index()
+ {
+    return "ini adalah halaman pengguna";
+ }
+ public function show()
+ {
+    return "ini adalah halaman profile user 6";
+ }
+}
