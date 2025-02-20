f63120c0e8b6b5e40d914d1e397c45cec0747774
Dwisna <158534538+Dwisna@users.noreply.github.com>
2025-02-20 22:30:42 +0700

    Update ManagementUserController.php

--- dwisna_projectmu/app/Http/Controllers/ManagementUserController.php
+++ dwisna_projectmu/app/Http/Controllers/ManagementUserController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class ManagementUserController extends Controller{
-    public function index(){
-        return "Hallo ini adalah method index, dalam controller management.";
+    public function index()
+    {
+        //return "Hallo ini adalah method index, dalam controller ManagementUser.";
+        //return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+        $nama = "Dwisna Risma Nur Hamida";
+        $pelajaran = ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman web"];
+        return view('home', compact('nama', 'pelajaran'));
     }
 
     public function create(){
