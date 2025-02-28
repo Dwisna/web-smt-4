b82bf5588d46b003e7b9f9bcb8cbad73c28d9841
Dwisna <158534538+Dwisna@users.noreply.github.com>
2025-02-28 10:42:19 +0700

    acara 9-11

--- dwisna_projectmu/app/Http/Controllers/Auth/LoginController.php
+++ dwisna_projectmu/app/Http/Controllers/Auth/LoginController.php
@@ -4,37 +4,45 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        $login = [
+            $loginType => $request->username,
+            'password' => $request->password,
+        ];
+
+        Log::info('Attempting login', $login);
+
+        if (Auth::attempt($login)) {
+            Log::info('Login successful for user: ' . $request->username);
+            return redirect()->route('home');
+        }
+
+        Log::warning('Login failed for user: ' . $request->username);
+        return redirect()->route('login')->with(['error' => 'Email/Password salah!']);
     }
 }
